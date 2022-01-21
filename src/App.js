7201126e01cd2d42d3346aa23475316a6b536899
Erik <slicedblue@gmail.com>
2022-01-21 01:06:26 -0500

    added word list // TODO: make backend for list instead of long array

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import React from 'react';
 class App extends React.Component {
   state = {
     letters: [["-","-","-","-","-"],["-","-","-","-","-"],["-","-","-","-","-"],["-","-","-","-","-"],["-","-","-","-","-"], ["-","-","-","-","-"]],
-    word: "acbdb",
+    list: ["rough","steep","twist","juicy","yummy","haunt","empty","harsh","sense","admit","scarf","cakes","field","waste","eager","aback","agree","jaded","round","blink","cheer","month","peace","nutty","windy","title","paint","burst","decay","glass","whirl","messy","kaput","fancy","crawl","exist","puffy","angry","zippy","overt","silky","crate","occur","stone","skate","young","jumpy","first","cloth","smash","tacky","punch","bumpy","grade","rural","paper","alike","brief","scene","quick","elfin","chief","taboo","tasty","shelf","curly","tough","crazy","funny","match","cagey","drunk","prose","witty","queue","cross","kitty","allow","coast","spade","shave","groan","hands","daily","sheet","order","rapid","blade","birds","carve","teeth","death","thick","cheat","short","girls","flesh","tooth","reach","equal","route","quiet","smart","grape","stiff","price","touch","abaft","trees","knife","stare","watch","taste","testy","found","wheel","ready","label","tiger","giant","awful","shirt","rings","fruit","raise","screw","swing","shake","jazzy","stage","happy","frail","north","clear","tease","cough","elite","treat","dance","cream","raspy","crash","dress","flaky","party","lunch","shape","ultra","gaudy","broad","cause","linen","glove","madly","lying","wrist","royal","sleet","spare","whine","sulky","earth","ratty","solid","horse","rebel","plant","drown","spill","shaky","board","trail","heady","white","thing","thumb","great","hurry","chalk","truck","juice","sugar","berry","crook","reign","error","prick","learn","noisy","floor","phone","crowd","brass","snore","smile","press","dream","jelly","laugh","guard","slimy","relax","pause","dirty","sable","proud","milky","spoil","unite","nerve","dolls","bored","loose","plane","wreck","claim","bless","metal","pushy","flock","scary","grain","elbow","place","offer","ducks","tangy","chess","spray","stuff","tempt","snake","quack","hover","scare","rabid","sound","share","itchy","known","utter","soggy","fetch","macho","weigh","lumpy","drain","tired","pedal","robin","silly","voice","drink","kneel","x-ray","aloof","stove","books","steam","color","frogs","frame","sheep","three","honey","spoon","actor","obese","river","legal","march","class","light","green","curve","mixed","murky","group","ocean","limit","house","birth","close","crown","wrong","spicy","third","daffy","plate","uncle","jeans","giddy","strap","trust","level","women","range","clean","guess","roomy","brave","force","money","whole","naive","cover","amuse","doubt","train","jewel","count","sassy","weary","value","front","coach","wacky","gusty","geese","right","scold","waves","knock","crime","blind","strip","grass","point","godly","nasty","moldy","erect","swift","black","verse","faint","curvy","stale","reply","tramp","rifle","misty","brush","start","early","dizzy","ritzy","alive","furry","spark","lucky","tight","slave","alarm","brick","zebra","scale","trick","skirt","bawdy","plain","enter","steer","smoke","argue","ruddy","aware","queen","dusty","rainy","mourn","space","trite","sniff","quilt","jolly","shock","nifty","ghost","scrub","grate","greet","event","sharp","spiky","bathe","zesty","basin","rinse","handy","fresh","boast","crush","false","night","smell","choke","power","mouth","super","brake","wound","marry","float","trade","cruel","quill","blush","nippy","anger","shrug","story","slope","paste","large","pinch","blood","avoid","crack","awake","woman","minor","spell","badge","delay","small","cable","chase","lowly","bells","annoy","songs","water","heavy","store","cycle","shoes","thank","brown","faded","vague","sleep","needy","sweet","noise","irate","straw","carry","magic","angle","trace","shiny","acrid","stain","guide","shade","teeny","fence","husky","flash","upset","woozy","shame","fuzzy","humor","flame","cheap","nappy","steel","print","gabby","worry","check","tense","snail","scent","eight","amuck","visit","rigid","rhyme","enjoy","alert","tacit","foamy","brash","meaty","serve","ahead","stick","stamp","burly","flood","bikes","salty","mushy","goofy","judge","table","fixed"],
+    word: "",
     count: 0,
     obtained: []
   }
 
   componentDidMount(){
+    this.setState({word: this.state.list[Math.floor(Math.random()*this.state.list.length)].toUpperCase()});
     var obtained = this.state.obtained;
-    for(let i in this.state.word) obtained.push("_");
+    for(var i = 0; i < 5; i++) obtained.push("_");
     this.setState({obtained});
 }
 
@@ -53,6 +55,12 @@ class App extends React.Component {
     else if(count === letters.length - 1){
       document.getElementById("result").innerHTML = "You did not find the word.";
       document.getElementById("input").disabled = true;
+
+      var obtained = this.state.obtained;
+      for(var i = 0; i < this.state.word.length; i++) {
+        obtained[i] = this.state.word[i];
+      }
+      this.setState({obtained});
     }
 
     // otherwise, reset the text box for new input
